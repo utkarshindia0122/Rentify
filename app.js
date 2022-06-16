826c00b3390c02b4476bc29cad7890d6b0250ac8
Utkarsh <utkarshindia0122@gmail.com>
2022-06-16 19:21:31 +0530

    added server and client side validations and added error handling

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const {houseSchema}=require('./schemas.js');
+const catchAsync=require('./utils/catchAsync');
+const ExpressError=require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const House = require('./models/house');
-
+//our database ,, database name -- rentify
 mongoose.connect('mongodb://localhost:27017/rentify', {
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -14,6 +17,7 @@ mongoose.connect('mongodb://localhost:27017/rentify', {
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
+     // console will show this mess age when database is connected
     console.log("Database connected");
 });
 
@@ -26,47 +30,82 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
+// this is a middlewhere which will check the incoming data is valid or not
+const validateHouse =(req,res,next)=>{
+    
+    const {error}=houseSchema.validate(req.body);
+
+    if(error){
+        const msg=error.details.map(el=>el.message).join(',')
+        throw new ExpressError(msg,400)
+    }
+    else{
+        next();
+    }
+}
 
 app.get('/', (req, res) => {
     res.render('home')
 });
-app.get('/houses', async (req, res) => {
+app.get('/houses', catchAsync(async (req, res) => {
     const houses = await House.find({});
     res.render('houses/index', { houses })
-});
+}));
 app.get('/houses/new', (req, res) => {
     res.render('houses/new');
 })
 
-app.post('/houses', async (req, res) => {
+// new house 
+/// catchAsync is from utils folder it will catch any error if mongoose throw an error
+app.post('/houses',validateHouse,catchAsync(async (req, res, next) => {
+    
     const house = new House(req.body.house);
     await house.save();
     res.redirect(`/houses/${house._id}`)
-})
+}))
 
-app.get('/houses/:id', async (req, res,) => {
+app.get('/houses/:id', catchAsync(async (req, res,) => {
+     // mongoose find the house by id and save to house
     const house = await House.findById(req.params.id)
+   // houses/show is render with house varible
     res.render('houses/show', { house });
-});
+}));
 
-app.get('/houses/:id/edit', async (req, res) => {
-    const house = await House.findById(req.params.id)
-    res.render('houses/edit', { house });
-})
+// edit form 
+app.get('/houses/:id/edit',catchAsync(async(req,res)=>{
+    // mongoose find the house by id and save to house
+    const house=await House.findById(req.params.id)
+    // houses/show is render with house varible
+    res.render('houses/edit',{house})
+}));
 
-app.put('/houses/:id', async (req, res) => {
-    const { id } = req.params;
-    const house = await House.findByIdAndUpdate(id, { ...req.body.house });
+
+// update route
+app.put('/houses/:id',validateHouse,catchAsync(async(req,res)=>{
+    const {id}=req.params;
+    //////////////////////////////////////         ... is spread operator  it itrate on req.body.house
+    const house =await House.findByIdAndUpdate(id,{...req.body.house});
     res.redirect(`/houses/${house._id}`)
-});
-
-app.delete('/houses/:id', async (req, res) => {
-    const { id } = req.params;
+}));
+// delete route
+app.delete('/houses/:id', catchAsync(async(req,res)=>{
+    const {id}=req.params;
     await House.findByIdAndDelete(id);
     res.redirect('/houses');
+}));
+
+///    .all is for all routes '*' will match any route 
+app.all('*',(req,res,next)=>{
+   next(new ExpressError('Page not Found',404));
 })
-
-
+// error hendeling
+app.use((err,req,res,next)=>{
+    // default status code =500 and default message ='Something went wrong'
+    const {statusCode =500}=err;
+    if(!err.message) err.message="Something Went Wrong!!!"
+    res.status(statusCode).render('error',{err});
+   
+})
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
---
+++ schemas.js
@@ -0,0 +1,12 @@
+const Joi=require('joi');
+
+module.exports.houseSchema=Joi.object({
+    house: Joi.object({
+        title: Joi.string().required(),
+        price: Joi.number().required().min(0),
+        image: Joi.string().required(),
+        location: Joi.string().required(),
+        description: Joi.string().required()
+    }).required()
+});
+
