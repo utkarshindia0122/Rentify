4e242f70268203b43bfc6e7bcb2aa24778108f44
Utkarsh <utkarshindia0122@gmail.com>
2022-06-19 13:23:22 +0530

    added image upload functionality

--- controllers/houses.js
+++ controllers/houses.js
@@ -1,33 +1,35 @@
 const House = require('../models/house');
-module.exports. index=async (req, res) => {
+module.exports.index = async (req, res) => {
     const houses = await House.find({});
     res.render('houses/index', { houses })
 }
-module.exports.renderNewForm=(req, res) => {
+module.exports.renderNewForm = (req, res) => {
     res.render('houses/new');
 }
-module.exports.createHouse=async (req, res, next) => {
+module.exports.createHouse = async (req, res, next) => {
     const house = new House(req.body.house);
+    house.images=req.files.map(f => ({ url: f.path, filename: f.filename }));
     house.author = req.user._id;
     await house.save();
+    console.log(house);
     req.flash('success', 'Successfully made a new house!');
     res.redirect(`/houses/${house._id}`)
 }
-module.exports.showHouse=async (req, res,) => {
+module.exports.showHouse = async (req, res,) => {
     const house = await House.findById(req.params.id).populate({
         path: 'reviews',
         populate: {
             path: 'author'
         }
     }).populate('author');
-    
+
     if (!house) {
         req.flash('error', 'Cannot find that house!');
         return res.redirect('/houses');
     }
     res.render('houses/show', { house });
 }
-module.exports.renderEditForm=async (req, res) => {
+module.exports.renderEditForm = async (req, res) => {
     const { id } = req.params;
     const house = await House.findById(id)
     if (!house) {
@@ -37,13 +39,13 @@ module.exports.renderEditForm=async (req, res) => {
     res.render('houses/edit', { house });
 }
 
-module.exports.updateHouse=async (req, res) => {
+module.exports.updateHouse = async (req, res) => {
     const { id } = req.params;
     const house = await House.findByIdAndUpdate(id, { ...req.body.house });
     req.flash('success', 'Successfully updated house!');
     res.redirect(`/houses/${house._id}`)
 }
-module.exports.deleteHouse=async (req, res) => {
+module.exports.deleteHouse = async (req, res) => {
     const { id } = req.params;
     await House.findByIdAndDelete(id);
     req.flash('success', 'Successfully deleted house')
--- routes/houses.js
+++ routes/houses.js
@@ -3,13 +3,17 @@ const router = express.Router();
 const houses=require('../controllers/houses');
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateHouse } = require('../middleware');
+const multer=require('multer');
+const {storage}=require('../cloudinary');
+const upload=multer({storage});
+
 
 const House = require('../models/house');
 
 router.route('/')
     .get(catchAsync(houses.index))
-    .post(isLoggedIn, validateHouse, catchAsync(houses.createHouse))
-
+    .post(isLoggedIn, upload.array('image'), validateHouse,catchAsync(houses.createHouse))
+    
 router.get('/new', isLoggedIn,houses.renderNewForm )
 
 router.route('/:id')
